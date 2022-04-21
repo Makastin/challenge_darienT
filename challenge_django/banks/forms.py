04c0b8e2bf636c95bee9d4f8417251014f56eeeb
Marcos Castellanos <makastin14@gmail.com>
2022-04-21 18:05:54 -0400

    add new file called forms to manage modelform in views

---
+++ challenge_django/banks/forms.py
@@ -0,0 +1,39 @@
+ ## Django packages ##
+
+from banks.models import Bank, BankType
+from django.forms import ModelForm
+from django.forms.widgets import (
+    CheckboxInput, 
+    Select,
+    TextInput,
+)
+
+
+## CreationForm of Model Bank ##
+class BankCreateForm(ModelForm):
+    class Meta:
+        model = Bank
+        fields = [
+            'name',
+            'bank_type',
+            'address',
+            'allowed_credit'
+        ]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'required': 'True'}),
+            'bank_type': Select(attrs={'class': 'form-control', 'required': 'True'}),
+            'address': TextInput(attrs={'class': 'form-control', 'required': 'True'}),
+            'allowed_credit': CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+        
+
+## CreationForm of Model BankType ##
+class BankTypeCreateForm(ModelForm):
+    class Meta:
+        model = BankType
+        fields = [
+            'bank_type'
+        ]
+        widgets = {
+            'bank_type': TextInput(attrs={'class': 'form-control', 'required': 'True'}),
+            }
