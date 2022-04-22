a7ea691e338f151e3468a9c829338841cb9e672a
Marcos Castellanos <makastin14@gmail.com>
2022-04-22 09:36:27 -0400

    changes in clients app

--- challenge_django/clients/urls.py
+++ challenge_django/clients/urls.py
@@ -11,19 +11,21 @@ from clients.views import (
         ClientDelet, 
         ClientDetail, 
         ClientList,
-        ClientUpdate
+        ClientUpdate,
+        ClientTypeCreate
 )
 
 urlpatterns = [
     re_path('create-client/$',
-            login_required(ClientCreate.as_view()), name="client-create"),      ## URL CLIENT CREATE
+            login_required(ClientCreate.as_view()), name="client-create"),                      ## URL CLIENT CREATE
     path('detail-client/<int:pk>/',
-            login_required(ClientDetail.as_view()), name="client-detail"),      ## URL CLIENT DETAIL
+            login_required(ClientDetail.as_view()), name="client-detail"),                      ## URL CLIENT DETAIL
     path('update-client/<int:pk>/',
-            login_required(ClientUpdate.as_view()), name="client-update"),      ## URL CLIENT UPDATE
+            login_required(ClientUpdate.as_view()), name="client-update"),                      ## URL CLIENT UPDATE
     re_path('list-client/$',
-            login_required(ClientList.as_view()), name="client-list"),          ## URL CLIENT LIST
+            login_required(ClientList.as_view()), name="client-list"),                          ## URL CLIENT LIST
     path('delet-client/<int:pk>/',
-            login_required(ClientDelet.as_view()), name="client-delet"),        ## URL CLIENT DELETE
-    
+            login_required(ClientDelet.as_view()), name="client-delet"),                        ## URL CLIENT DELETE
+    re_path('create-client-type/$',
+            login_required(ClientTypeCreate.as_view()), name="client-type-create"),             ## URL CLIENTTYPE CREATE
 ]
