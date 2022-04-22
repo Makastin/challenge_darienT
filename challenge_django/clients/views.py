e2e6e86d8867c68b9b59958a1ebd959dbcef6bd2
Marcos Castellanos <makastin14@gmail.com>
2022-04-21 21:10:39 -0400

    explain clients view

--- challenge_django/clients/views.py
+++ challenge_django/clients/views.py
@@ -5,6 +5,7 @@ from clients.forms import ClientFormCreate, ClientFormUpdate
 from django.urls import reverse_lazy
 
 
+## CREATEVIEW MODEL CLIENT ##
 class ClientCreate(CreateView):
     model = Client
     form_class = ClientFormCreate
@@ -14,11 +15,14 @@ class ClientCreate(CreateView):
     def form_valid(self, form):
         return super().form_valid(form)
 
-    
+
+## DETAILVUEW MODEL CLIENT ##
 class ClientDetail(DetailView):
     model = Client
     template_name = 'clients/client_detail.html'
     
+
+## UPDATEVIEW MODEL CLIENT ##
 class ClientUpdate(UpdateView):
     model = Client
     form_class = ClientFormUpdate
@@ -29,14 +33,17 @@ class ClientUpdate(UpdateView):
         return super().form_valid(form)
 
 
+## DELETEVIEW MODEL CLIENT
 class ClientDelet(DeleteView):
     model = Client
     success_url = reverse_lazy('clients:client-list')
 
+
+## LIST OF MODEL CLIENT ##
 class ClientList(TemplateView):
-   template_name = 'clients/client_list.html'
-   
-   def get(self, request, *args, **kwargs):
+    template_name = 'clients/client_list.html'
+    
+    def get(self, request, *args, **kwargs):
         client_list = Client.objects.prefetch_related('client_type', 'bank_entity').distinct()
         context = {'client_list': client_list}
         return self.render_to_response(context)
