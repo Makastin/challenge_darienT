9b47a89e5f941c66e4b05904ff7b1df4b56ac04b
Marcos Castellanos <makastin14@gmail.com>
2022-04-21 21:08:41 -0400

    adding queryset to show in conext

--- challenge_django/autentication/views.py
+++ challenge_django/autentication/views.py
@@ -3,6 +3,9 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
+from clients.models import Client
+from banks.models import Bank
+from credits.models import Credits
 
 
 ## Login View ##
@@ -10,6 +13,19 @@ class LoginView(LoginView):
     template_name = 'login/login.html'
     success_url = success_url = reverse_lazy('home')
 
+
 ## Home View ##
 class Home(TemplateView):
     template_name = "home/home.html"
+    
+    def get(self, request, *args, **kwargs):
+        clients = Client.objects.select_related('client_type', 'bank_entity').distinct().count()
+        banks = Bank.objects.select_related('bank_type').distinct().count()
+        credits = Credits.objects.select_related('client', 'bank_of_credit').distinct().count()
+        
+        context = {
+            'clients': clients,
+            'banks':banks,
+            'credits':credits
+        }
+        return self.render_to_response(context)
